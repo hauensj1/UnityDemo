2e1501b328fa52aeba958a291a6e43cd3b5a6ae1
Jacob Hauenstein <hauensj1@uah.edu>
2025-11-18 10:09:20 -0600

    Initial addition of Assets folder

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRigidbody;
+    public float speed = 20f;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    //void Update() {}
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        playerRigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    //OnCollision or OnTrigger...
+
+
+
+}
